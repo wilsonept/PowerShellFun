000f65b7e63fb9828f322d671abb7eef1c10edfb
wilsonept <wilsonept@gmail.com>
2020-12-25 22:17:10 +0300

    first commit

---
+++ Start-Music.ps1
@@ -0,0 +1,22 @@
+﻿$String = 'ПРИВЕТ АЛЕКСИЙ! ,,,,,,,,,,,,,,,,С НОВЫМ ГОДОМ ЙОПТА!'
+$Path = 'C:\Windows\Temp\Music.wav'
+
+function Start-Speech($String)
+{
+    Add-Type –AssemblyName System.Speech
+    $SpeechSynthesizer = [System.Speech.Synthesis.SpeechSynthesizer]::new()
+    $SpeechSynthesizer.SelectVoice('Microsoft Irina Desktop')
+    $SpeechSynthesizer.Speak($string)
+}
+
+function Start-Music($Path)
+{
+    $global:Player = New-Object -TypeName System.Media.SoundPlayer
+    $Player.SoundLocation = $Path
+    $Player.Play()
+    Write-Host 'что бы остановить музыку введите команду:'
+    Write-Host '$Player.Stop()' -ForegroundColor Green
+}
+
+Start-Speech -String $String
+Start-Music -Path $Path
---
+++ Start-Prank.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\WindowsPowershell\v1.0\Powershell.exe -ExecutionPolicy ByPass -WindowStyle Hidden -NoExit -NoProfile -File "C:\Windows\Temp\Start-Music.ps1"
